53a8a3d4933d010f6ed15194b64d2835bd798339
AmitabhaDazz <49267338+AmitabhaDazz@users.noreply.github.com>
2020-03-17 18:52:56 +0530

    Create awsdemo.tf

---
+++ awsdemo.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = ""
+  secret_key = ""
+}
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "dedicated"
+
+  tags = {
+    Name = "main"
+  }
+}
+resource "aws_subnet" "new"{
+  cidr_block="10.0.1.0/24"
+  vpc_id     = aws_vpc.main.id
+  }
