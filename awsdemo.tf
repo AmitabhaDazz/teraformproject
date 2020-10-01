1aa0a278d632ef298c2eaf4337d496dd9485a66f
Shivam Kumar <kumarshivam192@gmail.com>
2020-10-01 22:46:40 +0530

    Better way of Tags
    Please review my change

--- awsdemo.tf
+++ awsdemo.tf
@@ -6,12 +6,17 @@ provider "aws" {
 resource "aws_vpc" "main" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "dedicated"
+  tags = local.my_tags
+ }
 
-  tags = {
+locals {
+  my_tags = {
     Name = "main"
   }
 }
+
 resource "aws_subnet" "new"{
   cidr_block="10.0.1.0/24"
   vpc_id     = aws_vpc.main.id
+  tags = local.my_tags
   }
